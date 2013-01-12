75a094575371a96585d908c779094163d97b4acb
Josh Hubbard <jhubbardsf@gmail.com>
2013-01-11 19:03:21 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActorWebsite::Application.config.secret_token = '056309a0e70fcfee8c106814b49c4d04bd2048c0cff0217d49103d8c01b2294e3d922e0fa2403f11a1d259328dff5e9f85e03c516bc52b02dc81bf07f34729d3'
