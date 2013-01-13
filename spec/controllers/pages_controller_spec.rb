ba4a22e5edcd8e3737c38e5e6d20b196dbdf9ea4
Josh Hubbard <jhubbardsf@gmail.com>
2013-01-12 23:42:35 -0800

    Added some rspec tests.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Iris McLean"
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title)
     end
   end
 
@@ -14,12 +25,24 @@ describe PagesController do
       get 'bio'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'bio'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Biography")
+    end
   end
 
   describe "GET 'news'" do
     it "returns http success" do
       get 'news'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'news'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Latest News")
     end
   end
 
@@ -28,12 +51,24 @@ describe PagesController do
       get 'photos'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'photos'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Photos")
+    end
   end
 
   describe "GET 'videos'" do
     it "returns http success" do
       get 'videos'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'videos'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Videos")
     end
   end
 
@@ -42,12 +77,24 @@ describe PagesController do
       get 'resume'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'resume'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Resume")
+    end
   end
 
   describe "GET 'links'" do
     it "returns http success" do
       get 'links'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'links'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Links")
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,7 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+  require File.expand_path("../../config/environment", __FILE__)
 end
 
 Spork.each_run do
