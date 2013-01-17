0bd472a5347f0b79314733af769effd3a52252af
Josh Hubbard <jhubbardsf@gmail.com>
2013-01-16 21:43:40 -0800

    Removed ogg for production testing.

--- app/models/admin/video.rb
+++ app/models/admin/video.rb
@@ -2,7 +2,6 @@ class Admin::Video < ActiveRecord::Base
   attr_accessible :title, :user_id, :video_file
   has_attached_file :video_file, :styles => {
     :mp4 => { :geometry => "640x480", :format => 'mp4' },
-    :ogg => { :geometry => "640x480", :format => 'ogv' },
     :thumb  => { :geometry => "300x300", :format => 'jpg', :time => 30 }
   },                :processors      => [:ffmpeg]
 
