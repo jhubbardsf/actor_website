5a26e544a7c5335a5ea8806e1da9be09c1ea47a4
Josh Hubbard <jhubbardsf@gmail.com>
2013-01-18 10:58:40 -0800

    Removed video attachment from admin/videos table and model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117000849) do
+ActiveRecord::Schema.define(:version => 20130118185502) do
 
   create_table "admin_photos", :force => true do |t|
     t.string   "caption"
@@ -28,12 +28,8 @@ ActiveRecord::Schema.define(:version => 20130117000849) do
   create_table "admin_videos", :force => true do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-    t.string   "video_file_file_name"
-    t.string   "video_file_content_type"
-    t.integer  "video_file_file_size"
-    t.datetime "video_file_updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
