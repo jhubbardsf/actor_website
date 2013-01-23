c3a629dc495fa03ff6a09fd450a6032c531277a5
Josh Hubbard <jhubbardsf@gmail.com>
2013-01-23 12:17:52 -0800

    Worked on some fancybox bugs.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,46 +4,37 @@
 //= require fancybox
 
 $(function () {
-    var fancyBoxOptions = {
-        'autoScale': false,
-    }
+    var fancyBoxPictureOptions = {
+            'centerOnScroll': true
+        },
+        fancyBoxVideoOptions = {
+            'padding': 0,
+            'autoScale': false,
+            'transitionIn': 'fade',
+            'transitionOut': 'fade',
+            'width': 680,
+            'height': 495,
+            'type': 'swf',
+            'swf': {
+                'allowfullscreen': 'true'
+            }};
 
     $(document).on('click', 'a:not([data-pjax-ignore])', function (event) {
         var container = $('#content');
         $.pjax.click(event, container);
     });
 
-    $(document).on('click', '.main-links > a', function(event) {
+    $(document).on('click', '.main-links > a', function (event) {
         $('.main-links > a').removeClass('selected');
         $(this).addClass('selected');
     });
 
-    $('a.fancybox').fancybox(fancyBoxOptions);
-    $('a.fancybox-video').fancybox({
-        'padding': 0,
-        'autoScale': false,
-        'transitionIn': 'none',
-        'transitionOut': 'none',
-        'width': 680,
-        'height': 495,
-        'type': 'swf',
-        'swf': {
-            'allowfullscreen': 'true'
-        }});
+    $('a.fancybox').fancybox(fancyBoxPictureOptions);
+    $('a.fancybox-video').fancybox(fancyBoxVideoOptions);
 
     $(document).ajaxStop(function () {
-        $("a.fancybox").fancybox(fancyBoxOptions);
-        $("a.fancybox-video").fancybox({
-            'padding': 0,
-            'autoScale': false,
-            'transitionIn': 'none',
-            'transitionOut': 'none',
-            'width': 680,
-            'height': 495,
-            'type': 'swf',
-            'swf': {
-                'allowfullscreen': 'true'
-            }});
+        $("a.fancybox").fancybox(fancyBoxPictureOptions);
+        $("a.fancybox-video").fancybox(fancyBoxVideoOptions);
     });
 });
 
